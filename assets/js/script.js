9dfe29611f80bd20e297567fdc8770ce1112a7f7
Vinicius Soares Santos <100010777+vss-vinicius@users.noreply.github.com>
2023-10-14 00:08:57 -0300

    Update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () => {
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+    }
+
+});
